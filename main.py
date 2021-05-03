a09d51b90d7deeb086436e72eabaa155f95f9d23
ORLANDO CECHLAR BITENCOURT <orlando.cechlar@gmail.com>
2021-05-03 17:01:57 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ def gerar_senha(parametros_senha: dict) -> str:
     
     if parametros_senha['letras']:
         simbolos += ascii_letters
-    if parametros_senha['numbers']:
-        simbolos += "0123456789"*2
     if parametros_senha['digitos']:
         simbolos += digits
     if parametros_senha['ponctuation']:
@@ -20,15 +18,13 @@ def gerar_senha(parametros_senha: dict) -> str:
         return "".join(rand.choice(simbolos) for i in range(parametros_senha['tamanho']))
 
 def convert_data(data: str) -> dict:
-    info = dict(tamanho=20, letras=True, digitos=True, ponctuation=True, numbers=True)
+    info = dict(tamanho=20, letras=True, digitos=True, ponctuation=True)
 
     info['tamanho'] = int(data.split("&")[0].split("=")[1])
     
     if 'letras' not in data:
         info['letras'] = False
-    if 'numbers' not in data:
-        info['numbers'] = False
-    if 'digitos' in data:
+    if 'digitos' not in data:
         info['digitos'] = False
     if 'ponctuation' not in data:
         info['ponctuation'] = False
@@ -40,6 +36,7 @@ app = Flask(__name__)
 
 @app.route("/senha", methods=['GET', 'POST'])
 def senha():
+    parametros_senha = dict(tamanho=20, letras=True, digitos=True, ponctuation=True)
     senha="sua senha"
     if request.method == 'POST':
         data = request.get_data(as_text=True)
@@ -47,7 +44,7 @@ def senha():
         parametros_senha = convert_data(data)
 
         senha = gerar_senha(parametros_senha)
-    return render_template("index.html", senha=senha)
+    return render_template("index.html", senha=senha, parametros_senha=parametros_senha)
 
 
 @app.route("/", methods=['GET'])
